878ad1c7e3ec06a55d4de93aac38a8ae0984b6aa
karabo <kmodise682@icloud.com>
2025-07-28 16:20:54 +0200

    Added useState logic to Counter component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import "./App.css";
 
 function App() {
-  
+
+  const [counter,setCounter]=useState(0)
+  // Note for useState
+//1.counter - a variable which at first has a value of 0
+//2. setCounter - a function that can change counter  
 
   return (
     <>
        <div className='centerSection'>
          <h1>Counter</h1>
       <div className='counterSection'>
-      <button>+</button>
-      <h3>0</h3>
-      <button>-</button>
+      <button onClick={()=> setCounter(counter +1)}>+</button>
+      <h3>{counter}</h3>
+      <button onClick={()=> setCounter(counter -1)}>-</button>
 
       </div>
        </div>
