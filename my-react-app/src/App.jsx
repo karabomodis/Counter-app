daf9edf45c8ef59b0de1ff85e053452cf84ff599
karabo <kmodise682@icloud.com>
2025-07-28 14:47:56 +0200

    Initialize my-react-app with Vite, React, and ESLint setup
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to the public directory.
    - Created App.css for styling the application.
    - Implemented App.jsx as the main component with a basic counter layout.
    - Added React logo SVG to assets.
    - Created index.css for global styles.
    - Set up main.jsx for rendering the App component.
    - Configured Vite with vite.config.js for React support.

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+       <div className='centerSection'>
+         <h1>Counter</h1>
+      <div className='counterSection'>
+      <button>+</button>
+      <h3>0</h3>
+      <button>-</button>
+
+      </div>
+       </div>
+         
+      
+      
+    </>
+  )
+}
+
+export default App
