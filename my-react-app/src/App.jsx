2a0f99be542ba004b9f3870d69f17b6d22e16e0f
karabo <kmodise682@icloud.com>
2025-07-29 15:22:02 +0200

    added local storage support

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,25 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import "./App.css";
 
 function App() {
 
-  const [counter,setCounter]=useState(0)
+  const [counter, setCounter] = useState(0);
+  const [date, setDate] = useState(""); // Add date state
   // Note for useState
 //1.counter - a variable which at first has a value of 0
 //2. setCounter - a function that can change counter  
+
+useEffect( ()=>{
+  // typeof is used when you want to find out which datatype is being used
+  console.log(typeof(Number(localStorage.getItem("count"))))  
+  // the function Number is used to convert from string to number
+  // console.log(typeof(localStorage.getItem("count")))
+  setCounter(Number(localStorage.getItem("count")))
+
+},[])
+// the box brackets are used for the useEffect function to run once when the app is restarted
+
+function handleInc (){
+  setDate(new Date().toString())
+  localStorage.setItem("count", counter + 1)
+  setCounter(counter + 1)
+}
+
+function handleDec (){
+  localStorage.setItem("count",counter -1)
+  setCounter(counter -1)
+}
 
   return (
     <>
        <div className='centerSection'>
          <h1>Counter</h1>
       <div className='counterSection'>
-      <button onClick={()=> setCounter(counter +1)}>+</button>
+      <button onClick={handleInc}>+</button>
       <h3>{counter}</h3>
-      <button onClick={()=> setCounter(counter -1)}>-</button>
+      <button onClick={handleDec}>-</button>
 
       </div>
        </div>
          
+         <h3>{Date}</h3>
       
       
     </>
